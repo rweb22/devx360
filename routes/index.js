e2c8b062c0914d4b9995bf297b05e956704f8c87
rweb22 <nocomplexno@gmail.com>
2026-04-04 21:38:39 +0530

    Add debug endpoint to diagnose tenant detection

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+// Debug endpoint - check tenant detection
+router.get('/debug-tenant', (req, res) => {
+  res.json({
+    hostname: req.hostname,
+    subdomain: res.locals.subdomain,
+    tenant: req.tenant ? {
+      id: req.tenant.id,
+      name: req.tenant.name,
+      type: req.tenant.type,
+      subdomain: req.tenant.subdomain
+    } : null,
+    baseDomain: res.locals.app.baseDomain,
+    baseUrl: res.locals.app.baseUrl
+  });
+});
+
 // Home page
 router.get('/', (req, res) => {
   res.render('pages/home', {
